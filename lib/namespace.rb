2a08f66fe2e1fc1972900ffe3b6abad7ecacc9ef
Jonas Pfenniger <jonas@pfenniger.name>
2011-10-07 10:46:06 +0100

    Indent

--- lib/namespace.rb
+++ lib/namespace.rb
@@ -7,7 +7,7 @@ module Namespace
   class << self;
     attr_reader :cache
     attr_reader :load_path
-    
+
     # Loads a file according to the +namespace+ in the context of a module,
     # and returns that object.
     #
@@ -21,25 +21,25 @@ module Namespace
     # (require, load and include are already taken)
     def open(namespace)
       namespace = normalize(namespace)
-      
+
       # Cache lookup
       ns = @cache[namespace]
       return ns if ns
-  
+
       # File lookup
       file_path = namespace.gsub(':', File::SEPARATOR) + '.rb'
       file = @load_path.inject(nil) do |file, load_path|
         path = File.join(load_path, file_path)
         File.exists?(path) ? path : file
       end
-    
+
       raise ImportError, "no such file to load -- #{file_path}" unless file
-    
+
       file_content = File.read(file)
       # Pre-process __NAMESPACE__ keyword to act like __FILE__
       # in know... it's not exactly the same... (eg "__FILE__") but it should do the trick
       file_content.gsub!('__NAMESPACE__', namespace.inspect)
-    
+
       ns = Module.new
       ns.instance_variable_set("@__namespace__", namespace)
       # Allow calling methods in self, like in global namespace
@@ -47,33 +47,33 @@ module Namespace
       # Adds the #import methods from the DSL
       ns.extend(Namespace, Namespace::Ext)
       ns.module_eval(file_content, file)
-    
+
       # Cache
       @cache[namespace] = ns
-    
+
       return ns
     end
-    
+
     # Transforms a string into a normalized namespace identifier
     def normalize(namespace)
       namespace.to_s.sub(/^[\/:\s]*/, '').sub(/[\/:\s]*$/, '').gsub(/[\/\\:]+/, '::')
     end
-    
+
     # Returns the top name of a namespace
     #
     # Example: basename("some::deep::namespace") => "namespace"
     def basename(namespace)
       normalize(namespace).gsub(/.*::/,'')
     end
-    
+
     # Raises a ScriptError if the module is included
     def included(mod)
       raise ScriptError, "Namespace is not designed to be included, only extended. See #{mod}"
     end
     private :included
   end
-  
-  
+
+
   # options[:as] = select the imported name
   #
   # THINK: since variable is already returned, is :as necessary?
@@ -96,7 +96,7 @@ module Namespace
     alias to_s __namespace__
     private :__namespace__
   end
-  
+
   class ImportError < ScriptError; end
   class ConflictError < ScriptError; end
 end
